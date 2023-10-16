4d6d18a9201c930264b6895366384d625940f762
tienlee <vuletien2003@gmail.com>
2023-10-16 21:23:17 +0700

    tienlee

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,12 +1,22 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 'use client';
 import Image from 'next/image';
-
+import { Fragment, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react'
 export default function Home() {
+  let [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
   return (
     <div className={''}>
-      <main className={'container mx-auto mt-20 '}>
-        <div className="pt-10 grid gap-10 grid-cols-12 ">
+      <main className={'container mx-auto pt-10  '}>
+        <div className=" grid gap-10 grid-cols-12 ">
           {/* avatar */}
           <div className=" pt-5 col-span-2">
             <Image
@@ -25,7 +35,7 @@ export default function Home() {
             </p>
           </div>
           {/*  */}
-          <div className="pt-5 col-span-8">
+          <div className="pt-5 col-span-8 py-15">
             <p className="text-[30px] font-bold text-stone-800">Vũ Lê Tiến</p>
             {/* nhan day */}
             <div className=" flex flex-row border-b py-3">
@@ -152,7 +162,72 @@ export default function Home() {
             </div>
             {/*  */}
           </div>
-          <div className="mt-5 col-span-2">01</div>
+          <div className="mt-5 col-span-2"> 
+          <div className="">
+        <button
+          type="button"
+          onClick={openModal}
+          className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+        >
+          Open dialog
+        </button>
+      </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Payment successful
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      Your payment has been successfully submitted. We’ve sent
+                      you an email with all of the details of your order.
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      Got it, thanks!
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+          </div>
         </div>
       </main>
     </div>
