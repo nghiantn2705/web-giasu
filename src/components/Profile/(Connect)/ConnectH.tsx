e817fcdf4016ad482412dc41be3d7a750705f46e
Ngô Tiến Nghĩa <vuhung1067@gmail.com>
2023-12-09 02:32:10 +0700

    update

--- src/components/Profile/(Connect)/ConnectH.tsx
+++ src/components/Profile/(Connect)/ConnectH.tsx
@@ -9,8 +9,8 @@ interface IProps {
   infoUser: IUserInfo;
 }
 const ConnectH = ({ infoUser }: IProps) => {
-  const [data, setData] = useState<IConnect[]>([]);
-
+  const [data, setData] = useState<IConnect[]>();
+  console.log(data);
   useEffect(() => {
     (async () => {
       try {
@@ -24,6 +24,60 @@ const ConnectH = ({ infoUser }: IProps) => {
   return (
     <>
       {infoUser.role == 3 ? (
+        <div className={''}>
+          {data?.length ? (
+            <ul className={'flex flex-col gap-3 pl-0 mb-0 rounded-lg'}>
+              {data?.map((item, index: number) => {
+                return (
+                  <li
+                    key={index}
+                    className={
+                      'relative flex justify-between px-0 py-2 bg-white border-0 rounded-t-lg text-inherit'
+                    }
+                  >
+                    <div className={'flex'}>
+                      <picture
+                        className={
+                          'inline-flex items-center justify-center w-12 h-12 mr-4 text-white transition-all duration-200 text-base ease-soft-in-out rounded-xl'
+                        }
+                      >
+                        <img
+                          src={item?.userAvatar}
+                          alt="kal"
+                          className={
+                            'w-full drop-shadow-sm border p-1 rounded-xl'
+                          }
+                        />
+                      </picture>
+
+                      <div className={'flex flex-col items-start'}>
+                        <h6 className={'mb-0 leading-normal text-base'}>
+                          {item?.userName}
+                        </h6>
+                        <p className="flex gap-2 mb-0 leading-tight text-xs">
+                          {format(new Date(item?.created_at), 'HH:mm:ss')}
+                          <span>
+                            {format(new Date(item?.created_at), 'dd/MM/yyyy')}
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                    <div
+                      className={
+                        ' flex items-center text-sm before:h-2 before:w-2 before:rounded-full before:bg-green-500 before:mr-2'
+                      }
+                    >
+                      Hoàn thành
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            <p>Hiện chưa kết nối với ai</p>
+          )}
+        </div>
+      ) : (
         <div className={''}>
           <ul className={'flex flex-col gap-3 pl-0 mb-0 rounded-lg'}>
             {data?.map((item, index: number) => {
@@ -41,7 +95,7 @@ const ConnectH = ({ infoUser }: IProps) => {
                       }
                     >
                       <img
-                        src={item?.userAvatar}
+                        src={item?.teacherAvatar}
                         alt="kal"
                         className={
                           'w-full drop-shadow-sm border p-1 rounded-xl'
@@ -51,7 +105,7 @@ const ConnectH = ({ infoUser }: IProps) => {
 
                     <div className={'flex flex-col items-start'}>
                       <h6 className={'mb-0 leading-normal text-base'}>
-                        {item?.userName}
+                        {item?.teacherName}
                       </h6>
                       <p className="flex gap-2 mb-0 leading-tight text-xs">
                         {format(new Date(item?.created_at), 'HH:mm:ss')}
@@ -71,28 +125,6 @@ const ConnectH = ({ infoUser }: IProps) => {
                 </li>
               );
             })}
-          </ul>
-        </div>
-      ) : (
-        <div className={''}>
-          <ul className="flex flex-col gap-3 pl-0 mb-0 rounded-lg">
-            <li className="relative flex items-center px-0 py-2 bg-white border-0 rounded-t-lg text-inherit">
-              <div className="inline-flex items-center justify-center w-12 h-12 mr-4 text-white transition-all duration-200 text-base ease-soft-in-out rounded-xl">
-                <picture>
-                  <img
-                    src="https://demos.creative-tim.com/soft-ui-dashboard-tailwind/assets/img/kal-visuals-square.jpg"
-                    alt="kal"
-                    className="w-full shadow-soft-2xl rounded-xl"
-                  />
-                </picture>
-              </div>
-              <div className="flex flex-col items-start justify-center">
-                <h6 className="mb-0 leading-normal text-sm">Sophie B.</h6>
-                <p className="mb-0 leading-tight text-xs">
-                  Hi! I need more information..
-                </p>
-              </div>
-            </li>
           </ul>
         </div>
       )}
